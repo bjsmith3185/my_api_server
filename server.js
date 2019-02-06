05ff907ff88b4a847cb806e179816acdb8423462
bjsmith3185 <bjsmith3185@hotmail.com>
2019-02-06 15:08:32 -0500

    1

--- server.js
+++ server.js
@@ -17,9 +17,30 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Allow cors
+// app.use(cors({
+//   origin: "*"
+// }));
+
+
+var allowedOrigins = ['http://localhost:3000',
+                      'http://yourapp.com'];
 app.use(cors({
-  origin: "*"
+  origin: function(origin, callback){
+    // allow requests with no origin 
+    // (like mobile apps or curl requests)
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
 }));
+
+
+
+
 
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/practiceserver");
