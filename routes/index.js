2c713ba0fd27e8f0af5c36bfea6c55d29ec21bfb
bjsmith3185 <bjsmith3185@hotmail.com>
2019-02-08 19:12:27 -0500

    updated routes with err msg

--- routes/index.js
+++ routes/index.js
@@ -11,15 +11,12 @@ router.use("/api", apiRoutes);
 // Route to add data to api database
 router.use("/populate", populateRoutes);
 
-// Route to add data to api database
-router.use("/populate", populateRoutes);
-
 // Route to send api key
 router.use("/key", createKey);
 
 // If no API routes are hit, send the React app
 router.use("/", function (req, res) {
-    res.json(errorMSG);
+    res.json("Please read the docs at: https://bjsmith3185.github.io/My_API_homepage/");
 });
 
 module.exports = router;
