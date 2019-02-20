36df8ad23aeecbd650f788691a2e6abce63bc6dc
bjsmith3185 <bjsmith3185@hotmail.com>
2019-02-20 09:03:41 -0500

    added more entries to database

--- routes/setup/populateRouter.js
+++ routes/setup/populateRouter.js
@@ -15,36 +15,88 @@ const dataSeedArray = [
         first: "brian",
         last: "smith",
         age: 43,
-
     },
-
     {
         first: "emmett",
         last: "smith",
         age: 2,
-
     },
-
     {
-        first: "jessica",
-        last: "smith",
+        first: "brian",
+        last: "auston",
         age: 34,
-
     },
-
+    {
+        first: "jill",
+        last: "marsh",
+        age: 62,
+    },
+    {
+        first: "larry",
+        last: "waters",
+        age: 25,
+    },
+    {
+        first: "john",
+        last: "stokes",
+        age: 19,
+    },
+    {
+        first: "jonathan",
+        last: "perry",
+        age: 55,
+    },
+    {
+        first: "jack",
+        last: "long",
+        age: 25,
+    },
+    {
+        first: "robert",
+        last: "gormican",
+        age: 32,
+    },
+    {
+        first: "dave",
+        last: "myers",
+        age: 5,
+    },
+    {
+        first: "brian",
+        last: "luciano",
+        age: 2,
+    },
+    {
+        first: "nick",
+        last: "young",
+        age: 78,
+    },
+    {
+        first: "joe",
+        last: "armstrong",
+        age: 33,
+    },
+    {
+        first: "les",
+        last: "armstrong",
+        age: 56,
+    },
+    {
+        first: "justin",
+        last: "timberlake",
+        age: 76,
+    },
+    {
+        first: "steve",
+        last: "smith",
+        age: 21,
+    },
     {
         first: "tom",
         last: "petty",
-        age: 62,
-
+        age: 14,
     },
 
-    {
-        first: "police",
-        last: "man",
-        age: 25,
-
-    },
 ]
 
 
